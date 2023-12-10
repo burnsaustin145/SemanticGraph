fa8bea5de6dc8169b11483a61c7cb6b810a06345
burnsaustin145 <burnsaustin145@yahoo.com>
2023-12-09 19:16:14 -0500

    updating files

--- Main.hs
+++ Main.hs
@@ -9,10 +9,18 @@ loop graph = do
     putStrLn "Enter (add, relation, infer, reveal, query, q):"
     command <- getLine
     case words command of
-        ("add":ns) -> loop (addNode (read (head ns)) graph)
-        ("relation":ns) -> loop (addEdge (read (ns !! 0)) (read (ns !! 2)) (read (ns !! 1)) graph)
-        ("infer":ns) -> loop ((infer (read (ns !! 0)) (read (ns !! 1))) graph)
+        ("add":ns) -> if null ns  
+                      then putStrLn "Invalid command" >> loop graph
+                      else loop (addNode (head ns) graph)
+        ("relation":ns) -> if null ns || length ns /= 3
+                      then putStrLn "Invalid command" >> loop graph
+                      else loop (addEdge (ns !! 0) (ns !! 2) (ns !! 1) graph)
+        ("infer":ns) -> if null ns || length ns /= 2
+                      then putStrLn "Invalid command" >> loop graph
+                      else loop ((infer (ns !! 0) (ns !! 1)) graph)
         ("reveal":_)     -> (printGraph graph) >> loop graph
-        ("query":ns)  -> (queryGraph (read (ns !! 0)) graph) >> loop graph
+        ("query":ns)  -> if null ns || length ns /= 1
+                      then putStrLn "Invalid command" >> loop graph
+                      else (queryGraph (ns !! 0) graph) >> loop graph
         ("q":_)     -> return ()
         _              -> putStrLn "Invalid command" >> loop graph
